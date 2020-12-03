bac4144ceafcfe3d5b1c300566668369820cda37
Abdullah8313 <58742065+Abdullah8313@users.noreply.github.com>
2020-12-03 01:09:44 -0500

    Add files via upload

---
+++ Town_STUDENT_Test.java
@@ -0,0 +1,44 @@
+package cmsc204assignment6;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * CMSC 204 Professor Robert Alexander
+ * Town_STUDENT_Test.java
+ * @author Abdullah Nabeel
+ * 
+ */
+public class Town_STUDENT_Test {
+	private Town town;
+	@Before
+	public void setUp() throws Exception {
+		town = new Town("Town 1");
+		 
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		town = null;
+	}
+	
+	@Test
+	public void testCompareTo() {
+		assertEquals(0, town.compareTo(new Town("Town 1")));
+		assertEquals(0, town.compareTo(town));
+		assertEquals(-1, town.compareTo(new Town("Town 2")));
+	}
+	
+	@Test
+	public void testEquals() {
+		assertEquals(true, town.equals(new Town("Town 1")));
+		assertEquals(true, town.equals(town));
+	}
+	
+
+}
